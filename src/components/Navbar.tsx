b6b88af92c6e430612f353dbc2e8327dd414ab78
premprakashgupta <prem.com0011@gmail.com>
2025-07-22 06:24:49 +0530

    adding login method on login button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -22,6 +22,11 @@ export default function Navbar() {
     const [submitting, setSubmitting] = useState(false);
     const [submitted, setSubmitted] = useState(false);
 
+
+    const handleAdminLogin = () => {
+        window.location.href = 'https://vidya360.thundergits.com/admin';
+    };
+
     useEffect(() => {
         const handleScroll = () => {
             setScrolled(window.scrollY > 20);
@@ -145,6 +150,7 @@ export default function Navbar() {
                             Book a Demo
                         </button>
                         <button
+                            onClick={handleAdminLogin}
                             className="bg-white text-indigo-600 border border-indigo-600 px-5 py-2 rounded-lg font-semibold hover:bg-indigo-50 transition hover:scale-105 flex items-center gap-2"
                         >
                             <FiUser size={16} />
