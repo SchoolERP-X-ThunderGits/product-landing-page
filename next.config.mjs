db30b8e132dae5d2dbb7a51c74fac3234c329e07
root <root@srv1478375.hstgr.cloud>
2026-03-26 07:08:42 +0000

    remove export from edugit next website

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export', // Keep this line for static export
     images: {
       domains: ['images.unsplash.com'],
       unoptimized: true, // <-- ADD THIS LINE
